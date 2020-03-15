a27e7140f8712409578c4e4b63c720036b9a83f9
hyl2099 <hyl2099@gmail.com>
2020-03-15 17:16:39 +0100

    Change login update to use Sping API #75

--- src/app/core/login-dialog.component.ts
+++ src/app/core/login-dialog.component.ts
@@ -4,7 +4,6 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 
 import {TokensService} from './tokens.service';
 import {Staff} from '../home/staff/staff.model';
-import {isNotNullOrUndefined} from 'codelyzer/util/isNotNullOrUndefined';
 import {StaffService} from '../home/staff/staff.service';
 
 
@@ -20,6 +19,11 @@ export class LoginDialogComponent {
   nowTime: Date;
   newMobile: string;
   staff: Staff;
+  oldRecord: Staff[];
+  newRecord: Staff;
+  oldRecordeTime: Date;
+  itemId: string;
+  workHour: number;
 
   constructor(@Inject(MAT_DIALOG_DATA) data: any, private staffService: StaffService,
               private tokensService: TokensService, private router: Router) {
@@ -33,35 +37,48 @@ export class LoginDialogComponent {
 
     this.newMobile = this.mobile.toString();
     this.nowTime = new Date();
-    // if (
-    //     this.staffService.search(
-    //       this.newMobile,
-    //       this.nowTime.getFullYear().toString(),
-    //       (this.nowTime.getMonth() + 1).toString(),
-    //       this.nowTime.getDate().toString()
-    //     )._isScalar === true)
-    // {
-    //   this.staff = {
-    //     id : null,
-    //     mobile: this.newMobile,
-    //     year: (this.nowTime.getFullYear()).toString(),
-    //     month: (this.nowTime.getMonth() + 1).toString(),
-    //     day: this.nowTime.getDate().toString(),
-    //     workHours: null,
-    //     lastLoginTime: this.nowTime,
-    //   };
-    //   this.staffService.updateLoginRecord(this.staff);
-    // } else {
-    this.staff = {
-      id : null,
-      mobile: this.newMobile,
-      year: (this.nowTime.getFullYear()).toString(),
-      month: (this.nowTime.getMonth() + 1).toString(),
-      day: this.nowTime.getDate().toString(),
-      workHours: null,
-      lastLoginTime: this.nowTime,
-    };
-    this.staffService.createNewLoginRecord(this.staff).subscribe();
-    // }
+    this.staffService.search(
+      this.newMobile,
+      this.nowTime.getFullYear().toString(),
+      (this.nowTime.getMonth() + 1).toString(),
+      this.nowTime.getDate().toString())
+      .subscribe( data => {
+        this.oldRecord  = data;
+        console.log(this.oldRecord);
+        if (this.oldRecord.length > 0) {
+          this.nowTime = new Date();
+          this.oldRecordeTime =  new Date(this.oldRecord[0].lastLoginTime);
+          this.itemId = this.oldRecord[0].id;
+          this.workHour = this.oldRecord[0].workHours;
+          this.staff =  {
+            id : null,
+            mobile: this.newMobile,
+            year: (this.nowTime.getFullYear()).toString(),
+            month: (this.nowTime.getMonth() + 1).toString(),
+            day: this.nowTime.getDate().toString(),
+            workHours: this.workHour,
+            lastLoginTime: this.nowTime
+          };
+          this.staffService.updateLoginRecord(this.itemId, this.staff)
+            // tslint:disable-next-line:no-shadowed-variable
+            .subscribe(data => {
+                this.newRecord = data;
+                console.log(this.newRecord);
+                console.log('finished..log out');
+              }
+            );
+        } else {
+          this.staff = {
+            id : null,
+            mobile: this.newMobile,
+            year: (this.nowTime.getFullYear()).toString(),
+            month: (this.nowTime.getMonth() + 1).toString(),
+            day: this.nowTime.getDate().toString(),
+            workHours: null,
+            lastLoginTime: this.nowTime,
+          };
+          this.staffService.createNewLoginRecord(this.staff).subscribe();
+        }
+      });
   }
 }
--- src/app/home/staff/staff.service.ts
+++ src/app/home/staff/staff.service.ts
@@ -9,17 +9,6 @@ import {AppEndpoints} from '../../app-endpoints';
   providedIn: 'root'
 })
 export class StaffService {
-  staffTest: Staff[] = [
-    {
-      id: '1',  mobile: '661',  year: '2020', month: '1',  day: '1',  workHours: 8,  lastLoginTime: new Date(2020, 1, 1, 9, 0, 0)
-    },
-    {
-      id : '2', mobile: '662',  year: '2020', month: '1',  day: '1',  workHours: 8,  lastLoginTime: new Date(2020, 1, 1, 9, 0, 0)
-    },
-    {
-      id : '2', mobile: '6',  year: '2020', month: '3',  day: '12',  workHours: 0,  lastLoginTime: new Date(2020, 3, 12, 9, 0, 0)
-    },
-  ];
 
   constructor(private httpService: HttpService) {
   }
@@ -40,7 +29,6 @@ export class StaffService {
     return this.httpService.get(AppEndpoints.STAFFS);
   }
 
-
   createNewLoginRecord(newLoginRecord: Staff): Observable<Staff> {
     return this.httpService.post(AppEndpoints.STAFFS, newLoginRecord);
   }
@@ -48,26 +36,4 @@ export class StaffService {
   updateLoginRecord(id: string, newLoginRecord: Staff): Observable<Staff> {
     return this.httpService.put(AppEndpoints.STAFFS + '/' + id, newLoginRecord);
   }
-
-
-
-  // updateLoginRecord(newLoginRecord: Staff): Observable<Staff> {
-  //   // tslint:disable-next-line:prefer-for-of
-  //   let flag = 0;
-  //   // tslint:disable-next-line:prefer-for-of
-  //   for (let i = 0; i < this.staffTest.length; i++) {
-  //     const data = this.staffTest[i];
-  //     if (data.mobile === newLoginRecord.mobile && data.month === newLoginRecord.month && data.day === newLoginRecord.day) {
-  //       if (isNotNullOrUndefined(newLoginRecord.workHours)) {
-  //         data.workHours = newLoginRecord.workHours;
-  //       }
-  //       data.lastLoginTime = newLoginRecord.lastLoginTime;
-  //       return of(data);
-  //     }
-  //     flag = flag + 1;
-  //   }
-  //   if (flag > 0) {
-  //     return null;
-  //   }
-  // }
 }
