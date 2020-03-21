c400ef99a44ad794ae9725f24bdf11a5f7c63058
Melany <melasimb@gmail.com>
2020-03-21 17:05:07 +0100

    Create dialog component to create users #102

---
+++ src/app/home/users/users-creation-dialog.component.ts
@@ -0,0 +1,37 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatSnackBar} from '@angular/material';
+import {UserService} from './user.service';
+import {User} from './user.model';
+
+@Component({
+  styleUrls: ['users-creation-dialog.component.css'],
+  templateUrl: `users-creation-dialog.component.html`
+})
+export class UsersCreationDialogComponent {
+
+  user: User = {
+    mobile: null,
+    username: null,
+    email: null,
+    dni: null,
+    address: null
+  };
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: any,
+              private dialog: MatDialog,
+              private message: MatSnackBar,
+              private userService: UserService) {
+  }
+
+  createUser() {
+    this.userService.create(this.user).subscribe(
+      () => this.dialog.closeAll()
+      , () => this.message.open('Ups, something bad happened', null, {
+        duration: 2000,
+      })
+      , () => this.message.open('User created successfully', null, {
+        duration: 2000,
+      })
+    );
+  }
+}
