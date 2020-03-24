5e905ebb378ab80df910fda282d920edbb1e975c
laucv <laura.canelada.velasco@alumnos.upm.es>
2020-03-24 20:51:23 +0100

    Refactor #116

--- src/app/home/orders/order-detail-dialog.component.ts
+++ src/app/home/orders/order-detail-dialog.component.ts
@@ -4,6 +4,8 @@ import {OrderDetails} from './orderDetails.model';
 import {OrderService} from './order.service';
 import {OrderLineDetail} from './OrderLineDetail.model';
 import {OrderCreationDialogComponent} from './order-creation-dialog.component';
+import {ProviderService} from '../shared/provider.service';
+import { Provider } from '../shared/provider.model';
 
 @Component({
   templateUrl: 'order-detail-dialog.component.html'
@@ -12,17 +14,22 @@ import {OrderCreationDialogComponent} from './order-creation-dialog.component';
 export class OrderDetailDialogComponent {
 
   order: OrderDetails = {id: null, description: null, provider: null, openingDate: null, closingDate: null, orderLines: []};
+  provider: Provider;
 
   title = 'Orders\' articles';
   columns = ['article', 'requiredAmount', 'finalAmount'];
   data: OrderLineDetail[];
 
   constructor(private dialog: MatDialog, private dialogRef: MatDialogRef<OrderDetailDialogComponent>, private message: MatSnackBar,
-              private orderService: OrderService, @Inject(MAT_DIALOG_DATA) public orderData: any) {
+              private orderService: OrderService, @Inject(MAT_DIALOG_DATA) public orderData: any,
+              private providerService: ProviderService) {
     this.orderService.get(orderData.orderId).subscribe(
       data => {
         this.order = data;
         this.data = this.order.orderLines;
+        this.providerService.readAll().subscribe(
+          dataProvider => this.provider = dataProvider.filter(value => value.id === this.order.provider)[0]
+        );
       }
     );
   }
--- src/app/home/orders/order-edition-dialog.component.ts
+++ src/app/home/orders/order-edition-dialog.component.ts
@@ -4,6 +4,8 @@ import {OrderDetails} from './orderDetails.model';
 import {OrderService} from './order.service';
 import {OrderLineDetail} from './OrderLineDetail.model';
 import {OrderCloseDialogComponent} from './order-close-dialog.component';
+import {ProviderService} from '../shared/provider.service';
+import {Provider} from '../shared/provider.model';
 
 @Component({
   templateUrl: 'order-edition-dialog.component.html'
@@ -12,17 +14,22 @@ import {OrderCloseDialogComponent} from './order-close-dialog.component';
 export class OrderEditionDialogComponent {
 
   order: OrderDetails = {id: null, description: null, provider: null, openingDate: null, closingDate: null, orderLines: []};
+  provider: Provider;
 
   title = 'Orders\' articles';
   columns = ['article', 'requiredAmount', 'finalAmount'];
   data: OrderLineDetail[];
 
   constructor(private dialog: MatDialog, private dialogRef: MatDialogRef<OrderEditionDialogComponent>, private message: MatSnackBar,
-              private orderService: OrderService, @Inject(MAT_DIALOG_DATA) public orderData: any) {
+              private orderService: OrderService, @Inject(MAT_DIALOG_DATA) public orderData: any,
+              private providerService: ProviderService) {
     this.orderService.get(orderData.orderId).subscribe(
       data => {
         this.order = data;
         this.data = [...data.orderLines];
+        this.providerService.readAll().subscribe(
+          dataProvider => this.provider = dataProvider.filter(value => value.id === this.order.provider)[0]
+        );
       }
     );
   }
@@ -30,7 +37,7 @@ export class OrderEditionDialogComponent {
   editOrder() {
     this.orderService.update(this.order).subscribe(
       value => {
-        this.message.open('Order deleted: ' + this.order.description, null, {
+        this.message.open('Order edited: ' + this.order.description, null, {
           duration: 2000,
         });
         this.dialogRef.close();
