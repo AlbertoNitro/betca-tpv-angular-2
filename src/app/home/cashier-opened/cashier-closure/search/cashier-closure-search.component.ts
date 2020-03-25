f4c1d796443e52eea816c644d7a3ee5e7faa3941
Mary-Tarapuez <55232443+Mary-Tarapuez@users.noreply.github.com>
2020-03-25 16:09:20 +0100

    Implement endpoint for Cashier Closure Search and subscribe to service add property lostCash in the model issue #117

--- src/app/home/cashier-opened/cashier-closure/search/cashier-closure-search.component.ts
+++ src/app/home/cashier-opened/cashier-closure/search/cashier-closure-search.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {CashierClosureSearch} from './cashier-closure-search.model';
 import {CashierClosureMocks} from './cashier-closure-mocks.service';
+import {CashierService} from '../../../shared/cashier.service';
 
 @Component({
   templateUrl: 'cashier-closure-search.component.html'
@@ -10,10 +11,11 @@ export class CashierClosureSearchComponent {
   modelCashierClosureSearch: CashierClosureSearch;
   title = 'Cashier Closure Search';
   // tslint:disable-next-line:max-line-length
-  columns = ['closureDateS', 'initialCash', 'salesCard', 'salesCash', 'usedVouchers', 'deposit', 'withdrawal', 'lostCard', 'finalCash'];
+  columns = ['closureDate', 'initialCash', 'salesCard', 'salesCash', 'usedVouchers', 'deposit', 'withdrawal', 'lostCard', 'lostCash', 'finalCash'];
   data: CashierClosureSearch[];
+  minDate: Date;
 
-  constructor(private cashierClosureMocks: CashierClosureMocks) {
+  constructor(private cashierService: CashierService) {
     this.modelCashierClosureSearch = {
       // tslint:disable-next-line:max-line-length
       closureDate: null,
@@ -26,14 +28,24 @@ export class CashierClosureSearchComponent {
       deposit: null,
       withdrawal: null,
       lostCard: null,
+      lostCash: null,
       finalCash: null,
       comment: null
     };
+    this.data = null;
+    this.minDate = new Date(2020, 1, 1);
   }
   search() {
-    this.cashierClosureMocks.getAll().subscribe(
-      data => {this.data = [...data];
-      }
-      );
+    if (this.modelCashierClosureSearch.closureDate == null && this.modelCashierClosureSearch.finalCash == null) {
+    this.cashierService.readAll().subscribe(
+      data => this.data = data
+    );
+    }
   }
+   // search() {
+    // this.cashierClosureMocks.getAll().subscribe(
+     // data => {this.data = [...data];
+     // }
+     // );
+  // }
 }
