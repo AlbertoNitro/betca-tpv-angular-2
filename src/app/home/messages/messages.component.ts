4aa38560e8686a21509a83356bf35410ced093e1
Nerok96 <nerok96@gmail.com>
2020-03-28 18:48:22 +0100

    Old messages view

---
+++ src/app/home/messages/messages.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+
+export class MessagesComponent {
+
+  constructor() {
+  }
+
+  // TODO all functions related to reading al messages
+}
