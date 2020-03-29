7313e669ad70ee3867570368b4d450b456ff5ad7
Nerok96 <nerok96@gmail.com>
2020-03-29 21:22:18 +0200

    Changes to main message component to work similar to user component

--- src/app/home/messages/messages.component.ts
+++ src/app/home/messages/messages.component.ts
@@ -1,22 +1,54 @@
 import {Component} from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
+
+import {MessagesService} from './messages.service';
 import {Messages} from './messages.model';
+import {MatDialog} from '@angular/material';
+import {TokensService} from '../../core/tokens.service';
+import {MessagesCreationDialogComponent} from './messages-creation-dialog.component';
+import {MessagesDetailDialogComponent} from './messages-detail-dialog.component';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  templateUrl: 'messages.component.html',
 })
 
 export class MessagesComponent {
 
-  title = 'Old Messages';
-  columns: Array<string>;
-  dataSource: MatTableDataSource<Messages>;
+  messages: Messages;
+  title: string;
+  username: string;
+  columns: ['To Username', 'Send Date', 'Message content'];
+  data: Messages[];
 
-  constructor() {
-    // TODO Create Messages Services, then make a call once the user clicks on messages component
-    this.columns = ['Username', 'Message content'];
-    this.dataSource = new MatTableDataSource<Messages>(null);
+  constructor(private dialog: MatDialog, private messagesService: MessagesService, private tokensService: TokensService) {
+    this.username = tokensService.getName();
+    this.title = 'Old Messages sent to ' + this.username.toUpperCase();
+    this.messages = {
+      id: null,
+      fromUsername: null,
+      toUsername: null,
+      messageContent: null,
+      sentDate: null,
+      readDate: null
+    };
+
+    this.messagesService.readAll(this.username).subscribe(
+      data => this.data = data
+    );
+  }
+
+  create() {
+    this.dialog.open(MessagesCreationDialogComponent, {
+      data: {
+        update: false
+      }
+    });
+  }
+
+  read(messages: Messages) {
+    this.dialog.open(MessagesDetailDialogComponent, {
+      data: {
+        id: messages.id
+      }
+    });
   }
 }
