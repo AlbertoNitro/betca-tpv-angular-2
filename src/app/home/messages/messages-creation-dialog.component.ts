bcb9014fb227f64ac11d3a966a278bea5fbf2fe5
Nerok96 <nerok96@gmail.com>
2020-03-29 21:17:00 +0200

    Message creation dialog refactored

---
+++ src/app/home/messages/messages-creation-dialog.component.ts
@@ -0,0 +1,64 @@
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialog, MatSnackBar} from '@angular/material';
+import {MessagesService} from './messages.service';
+import {Messages} from './messages.model';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  styleUrls: ['messages-creation-dialog.component.css'],
+  templateUrl: `messages-creation-dialog.component.html`
+})
+export class MessagesCreationDialogComponent {
+
+  messages: Messages = {
+    id: null,
+    toUsername: null,
+    fromUsername: null,
+    messageContent: null,
+    sentDate: null,
+    readDate: null
+  };
+
+  toUsernameFormControl = new FormControl('', [Validators.required]);
+  messageContentFormControl = new FormControl('', [Validators.required]);
+
+  constructor(@Inject(MAT_DIALOG_DATA) data: any,
+              private dialog: MatDialog,
+              private message: MatSnackBar,
+              private messagesService: MessagesService) {
+  }
+
+
+  getErrorMessageToUserName() {
+    return this.toUsernameFormControl.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  getErrorMessageMessageContent() {
+    return this.messageContentFormControl.hasError('required') ? 'You must enter a value' : '';
+  }
+
+  createMessages() {
+    this.messagesService.create(this.messages).subscribe(
+      () => this.dialog.closeAll()
+      , error => this.errorControl(error.error)
+      , () => this.message.open('Message sent successfully', null, {
+        duration: 2000,
+      })
+    );
+  }
+
+  invalidMessages(): boolean {
+    return this.toUsernameFormControl.hasError('required') ||
+      this.messageContentFormControl.hasError('required');
+  }
+
+  errorControl(error: string) {
+    let messageError;
+    if (error) {
+      messageError = 'Ups, something bad happened';
+    }
+    this.message.open(messageError, null, {
+      duration: 2000,
+    });
+  }
+}
