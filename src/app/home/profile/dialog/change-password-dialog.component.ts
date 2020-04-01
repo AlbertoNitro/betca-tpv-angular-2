c2a6230b306fc171fbbb2d1f2cc5385dbbe53a97
María Sánchez <masanchezve@gmail.com>
2020-04-01 14:17:20 +0200

    Added Validators and Errors to Dialog Component #140

--- src/app/home/profile/dialog/change-password-dialog.component.ts
+++ src/app/home/profile/dialog/change-password-dialog.component.ts
@@ -1,19 +1,16 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {UserService} from '../../shared/users/user.service';
 import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, Validators} from '@angular/forms';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   templateUrl: 'change-password-dialog.component.html',
+  styleUrls: ['change-password-dialog.component.css']
 })
 export class ChangePasswordDialogComponent implements OnInit {
-
-  passwordGroup2 = new FormGroup({
-    password: new FormControl(''),
-    new_password: new FormControl(''),
-    confirm_new_password: new FormControl('')
-  });
-
+  mobile: number;
+  passwordUser: string;
   passwordGroup = this.fb.group({
     password: ['', [Validators.required]],
     new_password: ['', [Validators.required]],
@@ -22,14 +19,47 @@ export class ChangePasswordDialogComponent implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) data: any,
               private dialog: MatDialog,
+              private message: MatSnackBar,
               private userService: UserService,
               private fb: FormBuilder) {
-
+    this.mobile = data.mobile;
+    this.userService.getPassword(this.mobile).subscribe(
+      password => {
+        this.passwordUser = password;
+      }
+    );
   }
 
   ngOnInit() {
   }
 
   changePassword() {
+    this.userService.updatePassword(this.mobile, this.passwordUser).subscribe(
+      () => this.message.open('Password updated successfully', null, {
+        duration: 2500,
+      })
+    );
   }
+
+  passwordIncorrect(): boolean {
+    const passFormControl = this.passwordGroup.get('password');
+    if (passFormControl.touched) {
+      return passFormControl.value !== this.passwordUser;
+    }
+    return false;
+  }
+
+  passwordsDifferent() {
+    const newPassFormControl = this.passwordGroup.get('new_password');
+    const confirmNewPassFormControl = this.passwordGroup.get('confirm_new_password');
+    if (newPassFormControl.touched && confirmNewPassFormControl.touched) {
+      return newPassFormControl.value !== confirmNewPassFormControl.value;
+    }
+    return false;
+  }
+
+  formIsValid(): boolean {
+    return this.passwordGroup.valid && !this.passwordsDifferent() && !this.passwordIncorrect();
+  }
+
 }
