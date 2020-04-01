fc4233ac97af1f437fdf7b338aefc1e9707d3cdd
Sonia Béjar Muñoz <Sonia.Bejar017@gmail.com>
2020-04-01 13:01:14 +0200

    Add search bar to stock management component #152

--- src/app/home/stock-management/stock-management.component.ts
+++ src/app/home/stock-management/stock-management.component.ts
@@ -1,8 +1,32 @@
 import {Component} from '@angular/core';
+import {Article} from '../shared/article.model';
+import {StockMocksService} from './stock-mocks.service';
 
 @Component({
   templateUrl: 'stock-management.component.html',
   styleUrls: ['stock-management.component.css'],
 
 })
-export class StockManagementComponent {}
+export class StockManagementComponent {
+  articles: Article[];
+  stockManagementSearch: {
+    initDate: Date;
+    endDate: Date;
+    minimumStock: number;
+  };
+
+  constructor(private stockService: StockMocksService) {
+    this.initDefaultSearch();
+  }
+
+  search(): void {
+  }
+
+  initDefaultSearch(): void {
+    this.stockManagementSearch = {
+      initDate: null,
+      endDate: null,
+      minimumStock: null
+    };
+  }
+}
