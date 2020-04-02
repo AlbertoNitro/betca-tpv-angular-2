4d648f07b6cda55ec32a278956b23542701655e4
XinyuLi <911410598@qq.com>
2020-04-02 19:17:16 +0200

    Add Delete Sendings and Refactor updateSendings #164

--- src/app/home/sendings/sendings.component.ts
+++ src/app/home/sendings/sendings.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {Sendings} from './sendings.model';
 import {SendingsService} from './sendings.service';
-import {MatDialog} from '@angular/material';
+import {MatDialog, MatDialogConfig} from '@angular/material';
 import {SendingsCreationDialogComponent} from './sendings-creation-dialog.component';
 import {SendingsDetailDialogComponent} from './sendings-detail-dialog.component';
 import {SendingsUpdateDialogComponent} from './sendings-update-dialog.component';
+import {CancelYesDialogComponent} from '../../core/cancel-yes-dialog.component';
 
 @Component({
   templateUrl: 'sendings.component.html',
@@ -16,6 +17,7 @@ export class SendingsComponent {
   title = 'Delivery Management';
   columns = ['username', 'id', 'reference', 'creationDate', 'estado'];
   data: Sendings[];
+  dialogConfig: MatDialogConfig;
 
   constructor(private sendingsService: SendingsService, private dialog: MatDialog) {
     this.sendings = {username: null, id: null, reference: null, creationDate: null, estado: null};
@@ -66,6 +68,30 @@ export class SendingsComponent {
   }
 
   delete(sendings: Sendings) {
-    // TODO
+    this.dialogConfig = {
+      data: {
+        sendings
+      }
+    };
+    this.dialog.open(CancelYesDialogComponent, this.dialogConfig).afterClosed().subscribe(
+      result => {
+        if (result) {
+          this.sendingsService.delete(sendings).subscribe(response => {
+            if (response) {
+              this.readAll();
+            }
+          });
+        }
+      }
+    );
   }
+
+  readAll() {
+    this.sendingsService.readAll().subscribe(
+      result => {
+        this.data = result;
+      }
+    );
+  }
+
 }
